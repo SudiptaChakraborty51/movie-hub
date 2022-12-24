5f9c95191119cd08efaae79c5310a204918c0a35
Sudipta <hisudipta26@gmail.com>
2022-12-24 17:40:04 +0530

    fix: bottom navbar completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Header from "./Components/Header/Header";
 import SimpleBottomNavigation from './Components/MainNav';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container} from '@mui/material';
+import Trending from './Components/Pages/Trending/Trending';
+import Movies from './Components/Pages/Movies/Movies';
+import Series from './Components/Pages/Series/Series';
+import Search from './Components/Pages/Search/Search';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="App"></div>
+      <div className="App">
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending />} exact />
+            <Route path='/movies' element={<Movies />}/>
+            <Route path='/series' element={<Series />}/>
+            <Route path='/search' element={<Search />}/>
+          </Routes>
+        </Container>
+      </div>
       <SimpleBottomNavigation />
     </BrowserRouter>
   );
--- src/Components/MainNav.js
+++ src/Components/MainNav.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -9,6 +11,19 @@ import SearchIcon from '@mui/icons-material/Search';
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(value === 0){
+      navigate("/");
+    } else if (value===1) {
+      navigate("/movies");
+    } else if (value===2) {
+      navigate("/series");
+    } else if (value===3) {
+      navigate("/search");
+    }
+  }, [value, navigate]);
 
   return (
     <Box sx={{ width: "100%" ,
