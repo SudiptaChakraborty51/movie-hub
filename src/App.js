d1b2626e06affb82935cee8edaa10c831ae2f9b4
Sudipta <hisudipta26@gmail.com>
2022-12-24 14:31:04 +0530

    feat: add header and bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from "./Components/Header/Header";
+import SimpleBottomNavigation from './Components/MainNav';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <div className="App"></div>
+      <SimpleBottomNavigation />
+    </>
   );
 }
 
