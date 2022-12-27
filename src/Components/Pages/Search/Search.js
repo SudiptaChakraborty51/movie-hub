e31075ddd928e0605d7cad257e93179db54674a6
Sudipta <hisudipta26@gmail.com>
2022-12-27 17:15:23 +0530

    feat: started search page

--- src/Components/Pages/Search/Search.js
+++ src/Components/Pages/Search/Search.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Button, Tab, Tabs, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const Search = () => {
+  const [type, setType] = useState(0);
+  const [page, setPage] = useState(1);
+
   return (
     <div>
-      <span className='pageTitle'>Search</span>
+      <ThemeProvider theme={darkTheme}>
+        <div style={{ display: "flex", margin: "15px 0" }}>
+          <TextField
+          style={{ flex: 1 }}
+          className='searchBox'
+          label="Search"
+          variant='filled'
+          // onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button variant='contained' style={{marginLeft: 10}}> <SearchIcon /> </Button>
+        </div>
+
+        <Tabs 
+          value={type} 
+          indicatorColor="primary" 
+          textColor='primary'
+          onChange={(event, newValue) =>{
+            setType(newValue);
+            setPage(1);
+          }}
+        >
+          <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+        </Tabs>
+      </ThemeProvider>
     </div>
   )
 }
