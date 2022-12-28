878b0edb9fd627a0c90729276948b2294134b058
Sudipta <hisudipta26@gmail.com>
2022-12-29 00:11:15 +0530

    modal

---
+++ src/Components/ContentModal/ContentModal.js
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import "./ContentModal.css";
+import axios from 'axios';
+import { img_500, unavailable, unavailableLandscape } from '../config/config'; 
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  boxShadow: 24,
+  p: 4,
+  width:"90%",
+  height: "80%",
+  bgcolor: '#228BE6',
+  border: '1px solid #021d37',
+  borderRadius: 10,
+};
+
+export default function ContentModal({ children, media_type, id}) {
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState();
+  const [video, setVideo] = useState();
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setContent(data);
+    // console.log(data);
+  };
+
+  const fetchVideo = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setVideo(data.results[0]?.key);
+  };
+
+  useEffect(() => {
+    fetchData();
+    fetchVideo();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <Button className='media' onClick={handleOpen}>{children}</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {content && (
+              <div className="ContentModal">
+                <img
+                  src={
+                    content.poster_path
+                      ? `${img_500}/${content.poster_path}`
+                      : unavailable
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal__portrait"
+                />
+                <img
+                  src={
+                    content.backdrop_path
+                      ? `${img_500}/${content.backdrop_path}`
+                      : unavailableLandscape
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal__landscape"
+                />
+                <div className="ContentModal__about">
+                  <span className="ContentModal__title">
+                    {content.name || content.title} (
+                    {(
+                      content.first_air_date ||
+                      content.release_date ||
+                      "-----"
+                    ).substring(0, 4)}
+                    )
+                  </span>
+                  {content.tagline && (
+                    <i className="tagline">{content.tagline}</i>
+                  )}
+
+                  <span className="ContentModal__description">
+                    {content.overview}
+                  </span>
+
+                  
+                </div>
+              </div>
+          )}
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/Components/SingleContent/SingleContent.js
+++ src/Components/SingleContent/SingleContent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./SingleContent.css";
 import { img_300, unavailable } from '../config/config';
 import { Badge } from '@mui/material';
+import ContentModal from '../ContentModal/ContentModal';
 
 const SingleContent = ({
     id,
@@ -12,7 +13,7 @@ const SingleContent = ({
     vote_average,
 }) => {
   return (
-    <div className='media'>
+    <ContentModal media_type={media_type} id={id}>
       <Badge badgeContent={vote_average} color={vote_average>6? 'primary':'secondary'}/>
       <img className='poster' src={poster? `${img_300}/${poster}`:unavailable } alt={title} />
       <b className='title'>{title}</b>
@@ -20,7 +21,7 @@ const SingleContent = ({
         {media_type === 'tv'?"TV Series":"Movie"}
         <span className='subTitle'>{date}</span>
       </span>
-    </div>
+    </ContentModal>
   )
 }
 
